c0dab01f4a59c277307823ef07d6b21a4eae6742
Steve Losh <steve@stevelosh.com>
2013-08-11 17:15:48 -0400

    Add doc publishing script.

---
+++ docs/publish.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+rm -rf ./build
+~/lib/virtualenvs/d/bin/d
+hg -R ~/src/sjl.bitbucket.org pull -u
+rsync --delete -a ./build/ ~/src/sjl.bitbucket.org/red-tape
+hg -R ~/src/sjl.bitbucket.org commit -Am 'red-tape: Update site.'
+hg -R ~/src/sjl.bitbucket.org push
+
