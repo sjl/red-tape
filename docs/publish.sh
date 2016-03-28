3c2ee4cafba3dd3d669106f726070c89fd35a9f4
Steve Losh <steve@stevelosh.com>
2016-03-28 17:59:26 +0000

    Fix publish script

--- docs/publish.sh
+++ docs/publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 rm -rf ./build
-~/lib/virtualenvs/d/bin/d
+~/.virtualenvs/d/bin/d
 hg -R ~/src/sjl.bitbucket.org pull -u
 rsync --delete -a ./build/ ~/src/sjl.bitbucket.org/red-tape
 hg -R ~/src/sjl.bitbucket.org commit -Am 'red-tape: Update site.'
